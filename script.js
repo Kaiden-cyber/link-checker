cea8db97e7bb8cbc484f6db6b23135f28f8be808
Kaiden <timm.kaiden09@gmail.com>
2025-01-06 16:33:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,11 @@ function back(){
 function next(){
     document.location.href = getValues()[1]
 }
-const shorterUrl = getValues()[1].split("//")[1].split("/")[0]
+const protocol = getValues()[1].split("//")[0].split(":")[0];
+const shorterUrl = getValues()[1].split("//")[1].split("/")[0];
 document.getElementById("message").innerText = "Are you sure you recognize: " + shorterUrl + "?";
+
+function scan(){
+    const  encodedUrl = encodeURIComponent(encodeURIComponent(getValues()[1]));
+    window.open("https://www.virustotal.com/gui/search/"+encodedUrl);
+}
